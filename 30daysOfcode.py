5f4d1fdc2c2d4eb5277285778511c11ab3996b19
Divya Kannan <mnavane@g.clemson.edu>
2018-08-19 17:58:26 -0400

    Update 30daysOfcode.py

--- 30daysOfcode.py
+++ 30daysOfcode.py
@@ -15,3 +15,63 @@ while True:
             print("Not found")
     except:
         break
+#===================================================================================================================================
+# Day 3: 
+'''
+Task 
+Given an integer, , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+'''
+
+if __name__ == '__main__':
+    N = int(input())
+    if N%2 == 0:
+        if N>=2 and N<=5:
+            print("Not Weird")
+        elif N>=6 and N<=20:
+            print("Weird")
+        elif N>=20:
+            print("Not Weird")
+    else:
+        print("Weird")
+#===================================================================================================================================
+# Day 2: 
+'''
+Task 
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result!
+'''
+
+def solve(meal_cost, tip_percent, tax_percent):
+    totalCost = round(meal_cost + meal_cost*tip_percent/100 + meal_cost*tax_percent/100)
+    print("The total meal cost is {} dollars.".format(totalCost))
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
+
+#===================================================================================================================================
+# Day 1: Data Types
+
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+secondInt = int(input())
+secondDouble = float(input())
+secondStr = str(input())
+# Print the sum of both integer variables on a new line.
+print(i+secondInt)
+# Print the sum of the double variables on a new line.
+print(d+secondDouble)
+# Concatenate and print the String variables on a new line
+print(s+secondStr)
+# The 's' variable above should be printed first.
