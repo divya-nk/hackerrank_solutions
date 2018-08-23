bc3474f15729f31f2cddb1d6765e1cf198cbba4f
Divya Kannan <mnavane@g.clemson.edu>
2018-08-23 16:36:05 -0400

    update with divmod

--- Python-practice/math.py
+++ Python-practice/math.py
@@ -7,3 +7,12 @@ m = int(input())
 
 print(pow(a,b)) # prints a^b
 print(pow(a,b,m)) # prints a^b mod m (i.e) a^b % m
+
+# Mod - Divmod
+
+ # divmod, which takes two arguments a and b and 
+ # returns a tuple containing the quotient of a/b  first and then the remainder.
+
+print(a//b) #returns only the quotient
+print(a%b) #returns only the remainder
+print(divmod(a,b)) # returns tuple containing quotient and remainder
